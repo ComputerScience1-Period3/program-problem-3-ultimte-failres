82a8e017daa3a48e3c3a9ad5ad467e1b201c8482
Blueward12 <31855508+Blueward12@users.noreply.github.com>
2017-10-16 09:34:04 -0500

    Update Three_Digit_Ascend_Descend_Selection.cpp
    Kevin Ward 10/16/17

--- Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection.cpp
+++ Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection/Three_Digit_Ascend_Descend_Selection.cpp
@@ -5,38 +5,43 @@ Figure out if a number is ascending or descending
 */
 // Libraries
 #include <iostream> // gives access to cin, cout, endl, <<, >>, boolalpha, noboolalpha
-#include <conio.h> // gives access to _kbhit() and _getch() for pause()
+#include <conio.h>
+#include <Windows.h>// gives access to _kbhit() and _getch() for pause()
 // Namespaces
 using namespace std; //*
 					 // Functions() 
-void pause() {
-	cout << "Press any key to continue . . .";
-	while (!_kbhit());
-	_getch();
-	cout << '\n';
-}
 // MAIN
+void pause() {
+
+}
 void main() {
-	cout << "Please enter a three digit number: ";
-	int x;//x is three digit number
-	cin >> x;
-	int A, B, C; //C is ones digit. B is tens digit. A is hundreds digit
-	C = x % 10; //Gets last digit
-	x /= 10; //Omits last digit of x
-	B = x % 10; //Gets last digit
-	x /= 10; //Omits last digit
-	A = x; //One digit left
-	if (A > B && B > C)
-	{
-		cout << "Descending" << endl; //If the numbers are in order from greatest to least, output is "Ascending"
-	}
-	else if (A < B && B < C)
-	{
-		cout << "Ascending" << endl; //If the numbers are in order from least to greatest, output  is "Descending"
-	}
-	else
-	{
-		cout << "Neither" << endl; //If neither case is true, output is "Neither"
+	for (int i = 0; i < 30; i++) { // creates the loop, making sure to stop after 30 times
+
+		int x;//x is three digit number
+		cout << "Please enter a three digit number : ";
+		cin >> x;
+		int A, B, C; //Defines variables A, B, and C, for each digit of the 3 digit number
+		C = x % 10; //Gets last digit
+		x /= 10; //Omits last digit of x
+		B = x % 10; //Gets last digit
+		x /= 10; //Omits last digit
+		A = x; //One digit left
+
+		if (A > B && B > C)
+		{
+			cout << "Your number, " << A << B << C << ", is descending" << endl; //If the numbers are in order from greatest to least, the computer tells you that the number you entered in descending.
+			Sleep(500);
+		}
+		else if (A < B && B < C)
+		{
+			cout << "Your number, " << A << B << C << ", is ascending" << endl; //If the numbers are in order from least to greatest, output  is "Your number, (number), is ascending. 
+			Sleep(500);
+		}
+		else
+		{
+			cout << "The number you entered, " << A << B << C << ", is neither ascending nor descending..." << endl; //If neither case is true, output is "Neither"
+			Sleep(500);
+		}
 	}
 	pause(); // Pauses to see the displayed text
 }
